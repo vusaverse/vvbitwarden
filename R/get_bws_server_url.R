c022ae565bfa8295327d3bdd7fde20b07abaa823
esr316 <h.b.bons@vu.nl>
2025-03-31 11:02:33 +0200

    add: added examples

--- R/get_bws_server_url.R
+++ R/get_bws_server_url.R
@@ -5,12 +5,14 @@
 #'
 #' @return A character string containing the BWS server URL.
 #' @examples
-#' Set an environment variable for the server URL (example value)
+#' \dontrun{
+#' # Set an environment variable for the server URL (example value)
 #' Sys.setenv(BWS_SERVER_URL = "https://vault.bitwarden.eu")
 #'
 #' # Retrieve the server URL
 #' url <- get_bws_server_url()
 #' print(url)
+#' }
 get_bws_server_url <- function() {
   bws_server_url <- Sys.getenv("BWS_SERVER_URL")
   if (bws_server_url == "") {
